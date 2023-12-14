bc3a6481d63d1351064ccb1b9d3eb3eb6317c27a
valderez <valderezpe@gmail.com>
2023-12-14 13:24:12 -0300

    feat: create project, Docker, Postgres

---
+++ src/main/java/com/valderez/br/app_loja/AppLojaApplication.java
@@ -0,0 +1,13 @@
+package com.valderez.br.app_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valderez/br/app_loja/AppLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valderez.br.app_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
